a7bcd3f2872808fb8e7ad3a313b5c76f46e63c27
Pierre Beaujean <pierreb24@gmail.com>
2021-07-03 15:06:55 +0200

    lint

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ if len(sys.argv) != 2:
     raise Exception('one argument expected')
 
 abbrv = Abbreviate.from_files('LTWA_20170914-modified.csv', 'stopwords.txt')
-p = Pattern.from_line('médic-	méd.	cat')
-print(p)
-print(abbrv._potential_matches('Médica'))
+# p = Pattern.from_line('médic-	méd.	cat')
+# print(p)
+# print(abbrv._potential_matches('Médica'))
 
 print(abbrv(sys.argv[1]))
