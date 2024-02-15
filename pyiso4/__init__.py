015f8d9cd1208ffe3fb60950fc29f844048f4cc4
Pierre Beaujean <pierreb24@gmail.com>
2024-02-15 10:20:15 +0100

    Bump version: 0.1.4 → 0.1.5

--- pyiso4/__init__.py
+++ pyiso4/__init__.py
@@ -2,7 +2,7 @@
 Abbreviate a scientific journal title following the ISO-4 rules
 """
 
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 __author__ = 'Pierre Beaujean'
 __maintainer__ = 'Pierre Beaujean'
 __email__ = 'pierre.beaujean@unamur.be'
